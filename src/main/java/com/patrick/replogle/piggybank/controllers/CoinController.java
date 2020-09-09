0fca67fbf2a761b290c39ef3a8f3119e16b53749
patrick-replogle <patrickr1138@gmail.com>
2020-09-09 16:58:20 -0700

    still working on stretch problem

--- src/main/java/com/patrick/replogle/piggybank/controllers/CoinController.java
+++ src/main/java/com/patrick/replogle/piggybank/controllers/CoinController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,7 +20,6 @@ public class CoinController
     @Autowired
     private CoinRepository emprepos;
 
-    // http://localhost:2019/total
     @GetMapping(value = "/total", produces = {"application/json"})
     public ResponseEntity<?> listAllCoins()
     {
@@ -39,13 +40,40 @@ public class CoinController
                 System.out.println(quantity + " " + c.getName());
             }
 
-            else if(quantity > 1)
+            else
             {
                 System.out.println(quantity + " " + c.getNameplural());
             }
         }
 
         System.out.printf("The piggy bank holds " + total);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    // Stretch problem endpoint: still work in progress
+    @GetMapping(value = "/money/{amount}", produces = {"application/json"})
+    public ResponseEntity<?> removeCoins(@PathVariable String amount)
+    {
+        List<Coin> myList = new ArrayList<>();
+
+        emprepos.findAll().iterator().forEachRemaining(myList::add);
+
+        myList.sort((c1, c2) -> (int)(c1.getValue() - c2.getValue()));
+
+        double total = 0;
+        for (Coin c : myList)
+        {
+            total += c.getQuantity() * c.getValue();
+        }
+
+        float str = Float.valueOf(amount.substring(1, amount.length() - 1));
+
+        if (total < str)
+        {
+            System.out.printf("Money not available");
+        }
+
+        //System.out.printf("The piggy bank holds " + total);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
